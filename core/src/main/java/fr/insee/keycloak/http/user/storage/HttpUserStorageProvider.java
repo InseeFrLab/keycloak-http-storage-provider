1b20fd1ddbeadfe73754c705846c4c5c3bc5bd11
Cédric Couralet <cedric.couralet@insee.fr>
2021-08-04 15:37:46 +0200

    Various enhancement

--- core/src/main/java/fr/insee/keycloak/http/user/storage/HttpUserStorageProvider.java
+++ core/src/main/java/fr/insee/keycloak/http/user/storage/HttpUserStorageProvider.java
@@ -1,8 +1,8 @@
 package fr.insee.keycloak.http.user.storage;
 
+import fr.insee.keycloak.http.user.storage.service.HttpStoreService;
 import java.util.Collections;
 import java.util.Set;
-
 import org.keycloak.component.ComponentModel;
 import org.keycloak.credential.CredentialInput;
 import org.keycloak.credential.CredentialInputUpdater;
@@ -15,66 +15,68 @@ import org.keycloak.storage.StorageId;
 import org.keycloak.storage.UserStorageProvider;
 import org.keycloak.storage.user.UserLookupProvider;
 
-import fr.insee.keycloak.http.user.storage.service.HttpStoreService;
-
 public class HttpUserStorageProvider
-        implements UserStorageProvider, UserLookupProvider, CredentialInputValidator, CredentialInputUpdater {
+    implements UserStorageProvider,
+        UserLookupProvider,
+        CredentialInputValidator,
+        CredentialInputUpdater {
 
-    HttpStoreService httpStore;
+  HttpStoreService httpStore;
 
-    public HttpUserStorageProvider(KeycloakSession session, ComponentModel model) {
-        httpStore = new HttpStoreService(session, model.getConfig(), model);
-    }
+  public HttpUserStorageProvider(KeycloakSession session, ComponentModel model) {
+    httpStore = new HttpStoreService(session, model.getConfig(), model);
+  }
 
-    @Override
-    public void close() {
-        // nothing to do
-    }
+  @Override
+  public void close() {
+    // nothing to do
+  }
 
-    @Override
-    public boolean supportsCredentialType(String credentialType) {
-        return PasswordCredentialModel.TYPE.equals(credentialType);
-    }
+  @Override
+  public boolean supportsCredentialType(String credentialType) {
+    return PasswordCredentialModel.TYPE.equals(credentialType);
+  }
 
-    @Override
-    public boolean isConfiguredFor(RealmModel realm, UserModel user, String credentialType) {
-        return PasswordCredentialModel.TYPE.equals(credentialType);
-    }
+  @Override
+  public boolean isConfiguredFor(RealmModel realm, UserModel user, String credentialType) {
+    return PasswordCredentialModel.TYPE.equals(credentialType);
+  }
 
-    @Override
-    public boolean isValid(RealmModel realm, UserModel user, CredentialInput credentialInput) {
-        System.out.println("Validating " + user.getUsername() + ":" + credentialInput.getChallengeResponse());
-        return httpStore.validateCredential(user.getUsername(), credentialInput.getChallengeResponse());
-    }
+  @Override
+  public boolean isValid(RealmModel realm, UserModel user, CredentialInput credentialInput) {
+    System.out.println(
+        "Validating " + user.getUsername() + ":" + credentialInput.getChallengeResponse());
+    return httpStore.validateCredential(user.getUsername(), credentialInput.getChallengeResponse());
+  }
 
-    @Override
-    public UserModel getUserById(String id, RealmModel realm) {
-        return httpStore.getById(StorageId.externalId(id));
-    }
+  @Override
+  public UserModel getUserById(String id, RealmModel realm) {
+    return httpStore.getById(StorageId.externalId(id));
+  }
 
-    @Override
-    public UserModel getUserByUsername(String username, RealmModel realm) {
-        return httpStore.getByUsername(username);
-    }
+  @Override
+  public UserModel getUserByUsername(String username, RealmModel realm) {
+    System.out.println("getUserByUsername() : " + username);
+    return httpStore.getByUsername(username);
+  }
 
-    @Override
-    public UserModel getUserByEmail(String email, RealmModel realm) {
-        return httpStore.getByEmail(email);
-    }
+  @Override
+  public UserModel getUserByEmail(String email, RealmModel realm) {
+    return httpStore.getByEmail(email);
+  }
 
-    @Override
-    public boolean updateCredential(RealmModel realm, UserModel user, CredentialInput input) {
-        return httpStore.updateCredential(user, input);
-    }
+  @Override
+  public boolean updateCredential(RealmModel realm, UserModel user, CredentialInput input) {
+    return httpStore.updateCredential(user, input);
+  }
 
-    @Override
-    public void disableCredentialType(RealmModel realm, UserModel user, String credentialType) {
-        //No-op
-    }
+  @Override
+  public void disableCredentialType(RealmModel realm, UserModel user, String credentialType) {
+    // No-op
+  }
 
-    @Override
-    public Set<String> getDisableableCredentialTypes(RealmModel realm, UserModel user) {
-        return Collections.emptySet();
-    }
-    
+  @Override
+  public Set<String> getDisableableCredentialTypes(RealmModel realm, UserModel user) {
+    return Collections.emptySet();
+  }
 }
--- core/src/main/java/fr/insee/keycloak/http/user/storage/utils/Utils.java
+++ core/src/main/java/fr/insee/keycloak/http/user/storage/utils/Utils.java
@@ -1,84 +1,94 @@
 package fr.insee.keycloak.http.user.storage.utils;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import org.apache.commons.lang.text.StrSubstitutor;
 import org.apache.http.client.HttpClient;
 import org.keycloak.broker.provider.util.SimpleHttp;
 
 public class Utils {
 
-    private Utils() {
+  private Utils() {}
 
+  public static SimpleHttp prepareRequest(
+      HttpClient client,
+      String method,
+      String baseUrl,
+      String path,
+      Map<String, String> headers,
+      String bodyType,
+      List<String> body,
+      Map<String, String> params) {
+    SimpleHttp request = null;
+    System.out.println(baseUrl + Utils.inject(path, params));
+    System.out.println(method);
+    switch (method) {
+      case "GET":
+        System.out.println(baseUrl + Utils.inject(path, params));
+        request = SimpleHttp.doGet(baseUrl + Utils.inject(path, params), client).acceptJson();
+        request = addHeaderAndBody(headers, bodyType, body, params, request);
+        break;
+      case "POST":
+        request = SimpleHttp.doPost(baseUrl + Utils.inject(path, params), client).acceptJson();
+        request = addHeaderAndBody(headers, bodyType, body, params, request);
+        break;
     }
+    return request;
+  }
 
-    public static SimpleHttp prepareRequest(HttpClient client, String method, String baseUrl, String path,
-            Map<String, String> headers, String bodyType, List<String> body, Map<String, String> params) {
-        SimpleHttp request = null;
-        switch (method) {
-            case "GET":
-                request = SimpleHttp.doGet(baseUrl + Utils.inject(path, params), client).acceptJson();
-                request = addHeaderAndBody(headers, bodyType, body, params, request);
-                break;
-            case "POST":
-                request = SimpleHttp.doPost(baseUrl + Utils.inject(path, params), client).acceptJson();
-                request = addHeaderAndBody(headers, bodyType, body, params, request);
-                break;
+  private static SimpleHttp addHeaderAndBody(
+      Map<String, String> headers,
+      String bodyType,
+      List<String> body,
+      Map<String, String> params,
+      SimpleHttp request) {
+    // Add header to request
+    if (headers != null) {
+      for (String key : new ArrayList<>(headers.keySet())) {
+        request = request.header(key, Utils.inject(headers.get(key), params));
+      }
+    }
+    System.out.println(bodyType);
+    switch (bodyType) {
+      case "None":
+        break;
+      case "application/json":
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+          JsonNode json = mapper.readTree(Utils.inject(body.get(0), params));
+          request = request.json(json);
+        } catch (IOException e) {
+          e.printStackTrace();
         }
-        return request;
-    }
-
-    private static SimpleHttp addHeaderAndBody(Map<String, String> headers, String bodyType, List<String> body,
-            Map<String, String> params, SimpleHttp request) {
-        // Add header to request
-        if (headers != null) {
-            for (String key : new ArrayList<>(headers.keySet())) {
-                request = request.header(key, Utils.inject(headers.get(key), params));
-            }
+        break;
+      case "application/x-www-form-urlencoded":
+        System.out.println(params);
+        System.out.println(body);
+        Map<String, String> bodyParams =
+            body.stream().collect(Collectors.toMap(s -> s.split("=")[0], s -> s.split("=")[1]));
+        for (String bodyParamsKey : bodyParams.keySet()) {
+          request = request.param(bodyParamsKey, inject(bodyParams.get(bodyParamsKey), params));
         }
-        System.out.println(bodyType);
-        switch (bodyType) {
-            case "None":
-                break;
-            case "application/json":
-                ObjectMapper mapper = new ObjectMapper();
-                try {
-                    JsonNode json = mapper.readTree(Utils.inject(body.get(0), params));
-                    request = request.json(json);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                break;
-            case "application/x-www-form-urlencoded":
-                System.out.println(params);
-                System.out.println(body);
-                Map<String, String> bodyParams = body.stream()
-                        .collect(Collectors.toMap(s -> s.split("=")[0], s -> s.split("=")[1]));
-                for (String bodyParamsKey : bodyParams.keySet()) {
-                    request = request.param(bodyParamsKey, inject(bodyParams.get(bodyParamsKey), params));
-                }
-                break;
-        }
-
-        return request;
+        break;
     }
 
-    /**
-     * Inject the params in the expression by replacing ${var} by the value of the
-     * key var contained in the Params Map
-     * 
-     * @param expr
-     * @param params
-     * @return
-     */
-    private static String inject(String expr, Map<String, String> params) {
-        return StrSubstitutor.replace(expr, params);
-    }
+    return request;
+  }
+
+  /**
+   * Inject the params in the expression by replacing ${var} by the value of the key var contained
+   * in the Params Map
+   *
+   * @param expr
+   * @param params
+   * @return
+   */
+  private static String inject(String expr, Map<String, String> params) {
+    return StrSubstitutor.replace(expr, params);
+  }
 }
